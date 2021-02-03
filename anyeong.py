a70b89b7e8e5176730176fe0c7a18442c97ed0b0
palletoon <77739485+palletoon@users.noreply.github.com>
2021-02-03 10:52:12 -0500

    Add files via upload

---
+++ anyeong.py
@@ -0,0 +1,11 @@
+from tkinter import*
+
+root = Tk()
+
+# Creating a Label Widget
+myLabel = Label(root, text="anyeong, sesangah!")
+# Shoving it onto the screen
+myLabel.pack()
+
+
+root.mainloop()
